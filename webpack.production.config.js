a0062756755b10fd5a0aee8ac14614f99bc0545b
Lan <gracia@devlevelup.com>
2017-12-07 20:44:44 -0500

    webpack production config

---
+++ webpack.production.config.js
@@ -0,0 +1,78 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var ExtractTextPlugin = require('extract-text-webpack-plugin');
+
+module.exports = {
+    entry: {
+        app: path.resolve(__dirname, 'app/index.jsx'),
+
+        vendor: [
+            'react',
+            'react-dom',
+            'react-redux',
+            'react-router',
+            'redux',
+            'es6-promise',
+            'whatwg-fetch',
+            'immutable'
+        ]
+    },
+    output: {
+        path: __dirname + "/build",
+        filename: "[name].[chunkhash:8].js",
+        publicPath: '/'
+    },
+
+    resolve:{
+        extensions:['', '.js','.jsx']
+    },
+
+    module: {
+        loaders: [
+            { test: /\.(js|jsx)$/, exclude: /node_modules/, loader: 'babel' },
+            { test: /\.less$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss!less') },
+            { test: /\.css$/, exclude: /node_modules/, loader: ExtractTextPlugin.extract('style', 'css!postcss') },
+            { test:/\.(png|gif|jpg|jpeg|bmp)$/i, loader:'url-loader?limit=5000&name=img/[name].[chunkhash:8].[ext]' },
+            { test:/\.(png|woff|woff2|svg|ttf|eot)($|\?)/i, loader:'url-loader?limit=5000&name=fonts/[name].[chunkhash:8].[ext]'}
+        ]
+    },
+    postcss: [
+        require('autoprefixer')
+    ],
+
+    plugins: [
+        // webpack 内置的 banner-plugin
+        new webpack.BannerPlugin("Copyright by wangfupeng1988@github.com."),
+
+        new HtmlWebpackPlugin({
+            template: __dirname + '/app/index.tmpl.html'
+        }),
+
+        new webpack.DefinePlugin({
+            'process.env':{
+                'NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+            }
+        }),
+
+        new webpack.optimize.OccurenceOrderPlugin(),
+
+        new webpack.optimize.UglifyJsPlugin({
+            compress: {
+                //supresses warnings, usually from module minification
+                warnings: false
+            }
+        }),
+
+        new ExtractTextPlugin('[name].[chunkhash:8].css'),
+
+        new webpack.optimize.CommonsChunkPlugin({
+            name: 'vendor',
+            filename: '[name].[chunkhash:8].js'
+        }),
+
+        new webpack.DefinePlugin({
+            __DEV__: JSON.stringify(JSON.parse((process.env.NODE_ENV == 'dev') || 'false'))
+        })
+    ]
+};
