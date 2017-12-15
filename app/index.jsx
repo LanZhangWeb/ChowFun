70a6d4643489292c427fc11e5247dcb0be3558e9
Lan <gracia@devlevelup.com>
2017-12-15 11:29:47 -0500

    add redux store to root

--- app/index.jsx
+++ app/index.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { hashHistory } from 'react-router';
+import { Provider } from 'react-redux';
+import configureStore from './store/configureStore';
 
 import RouteMap from './router/routeMap';
 
+const store = configureStore();
+
 ReactDOM.render(
-    <RouteMap history={hashHistory} />,
+    <Provider store={store}>
+        <RouteMap history={hashHistory} />
+    </Provider>,
     document.getElementById('root')
 );
