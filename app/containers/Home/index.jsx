b7206fd7563da75a962dcdb3a11958c039c68ca7
Lan <gracia@devlevelup.com>
2017-12-16 22:13:56 -0500

    Homepage with header and city.

---
+++ app/containers/Home/index.jsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import HomeHeader from '../../components/HomeHeader';
+
+class Home extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+
+    render() {
+        return (
+            <HomeHeader cityName={this.props.userinfo.cityName}/>
+        );
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        userinfo: state.userinfo
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+    }
+}
+
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Home);
