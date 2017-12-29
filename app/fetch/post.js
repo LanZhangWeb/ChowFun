d76304fae41053eafd798a38cef41e7f4effea6e
Lan <gracia@devlevelup.com>
2017-12-28 22:31:23 -0500

    fetch post method

---
+++ app/fetch/post.js
@@ -0,0 +1,30 @@
+import 'whatwg-fetch';
+import 'es6-promise';
+
+function obj2params(obj) {
+    var result = '';
+
+    for (var item in obj) {
+        result += '&' + item + '=' + encodeURIComponent(obj[item]);
+    }
+
+    if (result) {
+        result = result.slice(1);
+    }
+
+    return result;
+}
+
+export function post(url, paramsObj) {
+    var result = fetch(url, {
+        method: 'POST',
+        credentials: 'include',
+        headers: {
+            'Accept': 'application/json, text/plain, */*',
+            'Content-Type': 'application/x-www-form-urlencoded'
+        },
+        body: obj2params(paramsObj)
+    });
+
+    return result;
+}
