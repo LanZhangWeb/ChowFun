4f4ea4b9be84d2b740a7738a7ba30f64510e09fd
Lan <gracia@devlevelup.com>
2018-01-02 21:40:16 -0500

    ad data mock

---
+++ mock/home/ad.js
@@ -0,0 +1,32 @@
+module.exports = [
+    {
+        title: 'Snack',
+        img: 'http://pic.yupoo.com/devlevelup/H0toL3i5/medish.jpg',
+        link: 'http://www.devlevelup.com/'
+    },
+    {
+        title: 'Travel',
+        img: 'http://pic.yupoo.com/devlevelup/H0toJiPi/medish.jpg',
+        link: 'http://www.devlevelup.com/'
+    },
+    {
+        title: 'Car wash',
+        img: 'http://pic.yupoo.com/devlevelup/H0toHWCJ/medish.jpg',
+        link: 'http://www.devlevelup.com/'
+    },
+    {
+        title: 'Piano class',
+        img: 'http://pic.yupoo.com/devlevelup/H0toFd06/medish.jpg',
+        link: 'http://www.devlevelup.com/'
+    },
+    {
+        title: 'Movie',
+        img: 'http://pic.yupoo.com/devlevelup/H0toD45j/medish.jpg',
+        link: 'http://www.devlevelup.com/'
+    },
+    {
+        title: 'Toys',
+        img: 'http://pic.yupoo.com/devlevelup/H0toBlnx/medish.jpg',
+        link: 'http://www.devlevelup.com/'
+    }
+];
