a113f019e89db91bbc9f74fd5d0a1d933692b48b
Lan <gracia@devlevelup.com>
2018-01-04 14:29:11 -0500

    Recommend list mock data.

---
+++ mock/home/list.js
@@ -0,0 +1,45 @@
+module.exports = {
+    hasMore: true,
+    data: [
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H1ntMzVG/medish.jpg',
+            title: 'Burger king',
+            subTitle: 'Try our burger',
+            price: '18',
+            distance: '40m',
+            mumber: '389'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H1ntIwbM/medish.jpg',
+            title: 'Luxury buffet',
+            subTitle: '[NorthYork]Buffet',
+            price: '98',
+            distance: '60m',
+            mumber: '689'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H1ntF34d/medish.jpg',
+            title: 'Custom cloth',
+            subTitle: 'Up to 40% off',
+            price: '500',
+            distance: '180m',
+            mumber: '106'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H1ntzf9E/medish.jpg',
+            title: 'Wedding photo',
+            subTitle: 'Free trial',
+            price: '600',
+            distance: '600m',
+            mumber: '58'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H1ntCstS/medish.jpg',
+            title: 'Spicy hotpot combo',
+            subTitle: 'Try it for free',
+            price: '0',
+            distance: '80m',
+            mumber: '1426'
+        }
+    ]
+};
