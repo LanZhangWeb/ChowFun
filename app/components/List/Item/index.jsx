6757fd70e307aef6ac4da767986bb4372fe594c8
Lan <gracia@devlevelup.com>
2018-01-05 10:33:47 -0500

    Recommendation list

---
+++ app/components/List/Item/index.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import './style.less';
+
+class Item extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+    }
+
+    render() {
+        const data = this.props.data;
+        return (
+            <div className="list-item clear-fix">
+                <div className="item-img-container float-left">
+                    <img src={data.img} alt={data.title}/>
+                </div>
+                <div className="item-content">
+                    <div className="item-title-container clear-fix">
+                        <h3 className="float-left">{data.title}</h3>
+                        <span className="float-right">{data.distance}</span>
+                    </div>
+                    <p className="item-sub-title">
+                        {data.subTitle}
+                    </p>
+                    <div className="item-price-container clear-fix">
+                        <span className="price float-left">${data.price}</span>
+                        <span className="number float-right">Over {data.mumber} bought</span>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+}
+
+export default Item;
