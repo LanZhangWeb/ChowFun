c8ea17dc88899ad9345a0a47e3c72d63c46e41f4
Lan <gracia@devlevelup.com>
2018-02-05 21:51:36 -0500

    no message

--- app/components/HomeHeader/index.jsx
+++ app/components/HomeHeader/index.jsx
@@ -21,7 +21,10 @@ class HomeHeader extends React.Component {
                     </Link>
                 </div>
                 <div className="home-header-right float-right">
-                    <i className="icon-user"></i>
+                    <Link to="/login">
+                        <span>Login</span>
+                        <i className="icon-user"></i>
+                    </Link>
                 </div>
                 <div className="home-header-middle">
                     <div className="search-container">
