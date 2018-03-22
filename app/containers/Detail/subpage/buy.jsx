ebb26c2324bbca70bfbcc0103b3ddc963eb10c1e
Lan <gracia@devlevelup.com>
2018-03-21 21:56:46 -0400

    detail page: buy

---
+++ app/containers/Detail/subpage/buy.jsx
@@ -0,0 +1,99 @@
+import React from 'react';
+import PureRenderMixin from 'react-addons-pure-render-mixin';
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { hashHistory } from 'react-router';
+import BuyAndStore from '../../../components/BuyAndStore';
+import * as storeActionsFromFile from '../../../actions/store';
+
+class Buy extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+        this.shouldComponentUpdate = PureRenderMixin.shouldComponentUpdate.bind(this);
+        this.state = {
+            isStore: false
+        }
+    }
+
+    render() {
+        return (
+            <div>
+                <BuyAndStore isStore={this.state.isStore}
+                             buyHandle={this.buyHandle.bind(this)}
+                             storeHandle={this.storeHandle.bind(this)}/>
+            </div>
+        );
+    }
+
+    checkStoreState() {
+        const id = this.props.id;
+        const store = this.props.store;
+
+        store.some(item => {
+            if (item.id === id) {
+                this.setState({
+                    isStore: true
+                });
+
+                return true;
+            }
+        });
+    }
+
+    loginCheck() {
+        const id = this.props.id;
+        const userinfo = this.props.userinfo;
+        if (!userinfo.username) {
+            hashHistory.push('/Login/' + encodeURIComponent('/detail/' + id));
+            return false;
+        }
+        return true;
+    }
+
+    buyHandle() {
+        const loginFlag = this.loginCheck();
+        if (!loginFlag) {
+            return;
+        }
+
+        // Buy process
+
+        hashHistory.push('/User')
+    }
+
+    storeHandle() {
+        const loginFlag = this.loginCheck()
+        if (!loginFlag) {
+            return;
+        }
+
+        const id = this.props.id;
+        const storeActions = this.props.storeActions;
+        if (this.state.isStore) {
+            storeActions.rm({id: id})
+        } else {
+            storeActions.add({id: id})
+        }
+
+        this.setState({
+            isStore: !this.state.isStore
+        })
+    }
+}
+
+function mapStateToProps(state) {
+    return {
+        userinfo: state.userinfo,
+        store: state.store
+    }
+}
+
+function mapDispatchToProps(dispatch) {
+    return {
+        storeActions: bindActionCreators(storeActionsFromFile, dispatch)
+    }
+}
+export default connect(
+    mapStateToProps,
+    mapDispatchToProps
+)(Buy);
