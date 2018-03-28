9c63114419afe5ac3341cdbe05862ed6e01759f9
Lan <gracia@devlevelup.com>
2018-03-27 22:01:46 -0400

    mock details

---
+++ mock/detail/info.js
@@ -0,0 +1,8 @@
+module.exports = {
+    img: 'http://pic.yupoo.com/devlevelup/H2jE5jri/medish.jpg',
+    title: 'Best hot pot',
+    star: 4,
+    price: '65',
+    subTitle: 'Sichuan, Spicy hot pot',
+    desc: 'Open hour: 11am - 9pm <br> Phone: 647-933-2345 <br> You can order online'
+};
