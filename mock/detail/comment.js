381a2375d84c64e86b72891ef4c1e9d842c83889
Lan <gracia@devlevelup.com>
2018-04-04 22:27:30 -0400

    no message

---
+++ mock/detail/comment.js
@@ -0,0 +1,40 @@
+module.exports = {
+    hasMore: true,
+    data: [
+        {
+            username: '647***3355',
+            comment: 'Excellent, I love it!',
+            star: 5
+        },
+        {
+            username: '416***3452',
+            comment: 'Great lamb, delicious.',
+            star: 4
+        },
+        {
+            username: '416***1242',
+            comment: 'Great atmosphere。',
+            star: 4
+        },
+        {
+            username: '647***3980',
+            comment: 'Not clean, will not visit',
+            star: 2
+        },
+        {
+            username: '416***3565',
+            comment: 'Regular, nothing special.',
+            star: 3
+        },
+        {
+            username: '647***9879',
+            comment: 'Tastes terrific.',
+            star: 5
+        },
+        {
+            username: '416***7570',
+            comment: 'I will recommend it to my friends.',
+            star: 4
+        }
+    ]
+};
