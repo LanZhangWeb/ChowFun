dcee79a1856478669b692ca56f98bd1602704883
Lan <gracia@devlevelup.com>
2018-04-09 08:59:32 -0400

    mock data

---
+++ mock/orderlist/orderlist.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        id: Date.now(),
+        img: 'http://pic.yupoo.com/devlevelup/H1ntMzVG/medish.jpg',
+        title: 'Burger King',
+        count: 3,
+        price: '15',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'http://pic.yupoo.com/devlevelup/H1ntCstS/medish.jpg',
+        title: 'Spicy hot pot',
+        count: 1,
+        price: '25',
+        commentState: 0
+    },
+    {
+        id: Date.now(),
+        img: 'http://pic.yupoo.com/devlevelup/H1ntIwbM/medish.jpg',
+        title: 'Buffet',
+        count: 2,
+        price: '60',
+        commentState: 2
+    }
+];
---
+++ mock/search/list.js
@@ -0,0 +1,53 @@
+module.exports = {
+    hasMore: true,
+    data: [
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H2aMMJJU/medish.jpg',
+            title: 'Little Sheep Hot Pot',
+            subTitle: 'Mongolian style',
+            price: '50',
+            distance: '120m',
+            number: '389'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H2aMMyp9/medish.jpg',
+            title: 'Sichuan Hot Pot',
+            subTitle: '[North York]Buffet',
+            price: '60',
+            distance: '140m',
+            number: '689'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H2aMMoBR/medish.jpg',
+            title: 'Beijing Hot Pot',
+            subTitle: 'Best lamb',
+            price: '75',
+            distance: '160m',
+            number: '106'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H2aMMegc/medish.jpg',
+            title: 'Popular Pot',
+            subTitle: 'BBQ Bar',
+            price: '45',
+            distance: '380m',
+            number: '58'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H2aMM3gc/medish.jpg',
+            title: 'Loco Hot Pot',
+            subTitle: 'Noodle, Fast Food',
+            price: '45',
+            distance: '500m',
+            number: '689'
+        },
+        {
+            img: 'http://pic.yupoo.com/devlevelup/H2aMLSW8/medish.jpg',
+            title: 'Spicy Hot Pot',
+            subTitle: 'North York',
+            price: '60',
+            distance: '800m',
+            number: '58'
+        }
+    ]
+};
