998aa693e3de4a69d2c6b318657cc8eb36c9534d
Lan <gracia@devlevelup.com>
2018-04-30 21:46:21 -0400

    no message

--- app/router/routeMap.jsx
+++ app/router/routeMap.jsx
@@ -7,6 +7,7 @@ import User from '../containers/User';
 import City from '../containers/City';
 import Detail from '../containers/Detail';
 import Search from '../containers/Search';
+import Login from '../containers/Login';
 import NotFound from '../containers/404';
 
 class RouteMap extends React.Component {
@@ -19,6 +20,7 @@ class RouteMap extends React.Component {
                     <Route path='/user' component={User}/>
                     <Route path='/search/:category(/:keyword)' component={Search}/>
                     <Route path='/detail/:id' component={Detail}/>
+                    <Route path='/login(/:router)' component={Login}/>
                     <Route path='*' component={NotFound}/>
                 </Route>
             </Router>
